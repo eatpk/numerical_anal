c4fa8b613b108db4269da9475d0bd2ba2fe47f3b
eatpk <30644997+eatpk@users.noreply.github.com>
2018-03-21 15:37:40 +0900

    Add files via upload
    First lab for Numerical Analysis: Bisection Method, Newton Method, calculating x-sin(x) without losing the significance

---
+++ 1_4_3comp_prob.py
@@ -0,0 +1,22 @@
+'''
+[Computer Exercises 1.4.3] Alter and test the pseudocode in the text for computing x􀀀sin x by using nested
+multiplication to evaluate the series.
+'''
+
+import numpy as np
+
+def f(x):
+    n=10
+    if abs(x)>=1.9:
+        return x-np.sin(x)
+    else:
+        t= float((x**3)/6)
+        s=t
+        
+        for i in range(2,n):
+            t= float((-t*(x**2))/((2*i+2)*(2*i+3)))
+            s=s+t
+        return s
+        
+x=float(input("Calculating the value of x - sin(x), insert x:"))
+print(f(x))
