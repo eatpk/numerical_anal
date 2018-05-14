6947ee729e0173de0c27b736ee93bbeea45f960c
eatpk <30644997+eatpk@users.noreply.github.com>
2018-05-14 13:42:32 +0900

    Add files via upload

---
+++ lab8_RungeKutta.py
@@ -0,0 +1,47 @@
+import numpy as np
+import matplotlib.pyplot as plt
+#Runge-kutta 2 is...
+#x(t+h)=x(t)+0.5(K1+K2)
+#K1=hf(t,x)
+#K2=hf(t+h,x+K1)
+def f(t,x):
+    return 1+x**2+t**3
+
+
+
+def Runge_Kutta2(f,x,t,h):
+    K=np.zeros(2)
+    
+    K[0]=h*f(t,x)
+    K[1]= h*f(t+h,x+K[0])
+    
+    x1=x+0.5*(K[0]+K[1])
+    
+    return x1
+
+def Runge_Kutta4(f,x,t,h):
+    
+    K=np.zeros(4)
+    
+    K[0]=h*f(t,x)
+    K[1]=h*f(t+0.5*h,x+0.5*K[0])
+    K[2]=h*f(t+0.5*h,x+0.5*K[1])
+    K[3]=h*f(t+h,x+K[2])
+    
+    x1= x+(K[0]+2*K[1]+2*K[2]+K[3])/6
+    return x1
+a=1
+b=2#Domain [a,b]
+n=101
+x=np.zeros(n)
+t=np.linspace(a,b,n) #t= a + hi ->this is equivalent to linspace
+h=(b-a)/(n-1)
+
+x[0]=-4.0#initial value(boundary problem)
+
+for i in range(n-1):
+    x[i+1]= Runge_Kutta4(f,x[i],t[i],h)
+    
+plt.plot(t,x)
+plt.show()
+print("Value:",x[n-1])
---
+++ lab8_ode.py
@@ -0,0 +1,37 @@
+import numpy as np
+import matplotlib.pyplot as plt
+'''
+    x'=1+x^2+t^3
+    x''=2xx'+3t^2
+    x```=2xx``+2x`x`+6t
+    x````=2xx```+6x`x``+6
+    x(1)=4
+'''
+def taylor4(f0,t,h):
+    f=np.zeros(5)
+    
+    f[0]=f0
+    f[1]=1+f[0]**2 + t**3
+    f[2]=2*f[0]*f[1]+ 3*t**2
+    f[3]=2*f[0]*f[2]+2*f[1]**2+6*t
+    f[4]=2*f[0]*f[3]+6*f[1]*f[2]+6
+
+    taylor_expansion=f[0]+h*f[1]+h**2/2*f[2] + h**3/6 * f[3]+h**4/24 * f[4]#taylor expansion
+    
+    return taylor_expansion;
+
+a=1
+b=2#Domain [a,b]
+n=101
+x=np.zeros(n)
+t=np.linspace(a,b,n) #t= a + hi ->this is equivalent to linspace
+h=(b-a)/(n-1)
+
+x[0]=-4.0#initial value(boundary problem)
+
+for i in range(n-1):
+    x[i+1]= taylor4(x[i],t[i],h)
+    
+plt.plot(t,x)
+plt.show()
+print("Value:",x[n-1])
