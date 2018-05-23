fee8d11b9e9519526804dbdcd43664a0049785b8
eatpk <30644997+eatpk@users.noreply.github.com>
2018-05-23 21:50:17 +0900

    Add files via upload

---
+++ ComputerExercise_11_2_1.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+a=1
+b=2
+
+for n in [10,100,1000]:
+    A= np.matrix(np.zeros((n,n)))
+    B= np.matrix(np.zeros((n,1)))
+
+    A[0,0]=1
+    A[n-1,n-1]=1
+    B[0]= 0
+    B[n-1]= 1
+    h=(b-a) / (n-1)
+    for i in range(1,n-1):
+        t= a+i*h
+    
+        A[i,i-1]= (1/h**2 +np.cos(t)/(2*h))
+        A[i,i]= -(2/h**2 +np.sin(t))
+        A[i,i+1]= (1/h**2 -np.cos(t)/(2*h))
+        B[i]= -np.exp(t)
+ 
+    x=np.linalg.solve(A,B)
+    
+    t= np.linspace(a,b,n)
+
+
+    plt.plot(t,x,'o')
+    plt.show()
